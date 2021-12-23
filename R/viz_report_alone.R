6eeb98e83c1410ce1891e383cc0a59a1d834aa71
gertstulp <g.stulp@rug.nl>
2021-12-23 11:51:31 +0100

    updated functions added data

--- R/viz_report_alone.R
+++ R/viz_report_alone.R
@@ -6,17 +6,38 @@
 #' @export
 viz_report_alone <- function(data, output_format = "svg") {
 
-  g <- data %>% 
+  d <- data %>% 
     mutate(fase = factor(csp_dna_fase, levels = c(4, 3, 2, 1))) %>% 
     group_by(csp_dna_55_a0, fase, .drop = FALSE) %>% 
     summarise(n = n()) %>% 
     filter( !is.na(csp_dna_55_a0) & !is.na(fase) ) %>% 
     group_by(csp_dna_55_a0) %>% 
-    mutate(perc = 100 * n / sum(n)) %>% 
-    ggplot(aes(x = csp_dna_55_a0, y = perc, fill = fase)) +
+    mutate(perc = 100 * n / sum(n)) 
+  
+  lbls_alone <- d %>% 
+    group_by(csp_dna_55_a0) %>% 
+    summarise(n_alone = sum(n)) %>% 
+    mutate(n_tot = sum(n_alone),
+           perc =  round(100 * n_alone / n_tot),
+           label = case_when(csp_dna_55_a0 == 0 ~ "In gezelschap",
+                             csp_dna_55_a0 == 1 ~ "Alleen"),
+           lbl = paste0(label, " [", perc, "%]")) 
+  
+  lbls_fase <- d %>% 
+    group_by(fase) %>% 
+    summarise(n_fase = sum(n)) %>% 
+    mutate(n_tot = sum(n_fase),
+           perc =  round(100 * n_fase / n_tot),
+           lbl = paste0(fase, " [", perc, "%]")) %>% 
+    pull(lbl)
+
+  g <- ggplot(d, aes(x = csp_dna_55_a0, y = perc, fill = fase)) +
     geom_bar(position = "stack", stat = "identity") +
-    scale_x_continuous(breaks = c(0, 1), labels = c("In gezelschap", "Alleen")) +
+    scale_x_continuous(breaks = c(0, 1), 
+                       labels = c(filter(lbls_alone, csp_dna_55_a0 == 0)$lbl, 
+                                  filter(lbls_alone, csp_dna_55_a0 == 1)$lbl)) +
     scale_fill_manual(breaks = factor(c(1, 2, 3, 4)),
+                      labels = rev(lbls_fase),
                       values = c("1" = "green3", "2" = "yellow2",
                                  "3" = "darkorange", "4" = "firebrick2")) +
     theme_minimal() +
--- R/viz_report_behaviour.R
+++ R/viz_report_behaviour.R
@@ -1,53 +1,78 @@
-#' Creates first visualisation for feedback-report (response-rate)
+#' Creates average behaviour per phase for feedback-report
 #'
 #' @param data A dataframe with raw data
+#' @param output_format String ("svg" or "ggplot") defining whether output should be ggplot or svg
 #' @return An svg of visualisation
+#' @import ggplot2 dplyr
 #' @export
-viz_report_behaviour <- function(variable) {
+viz_report_behaviour <- function(data, output_format = "svg") {
 
-  # ### Figuur 1
-  # Pp001 begeeft zich het meest in fase 2 en daarna fase 3. Slechts 1 keer begeeft pp001 zich in fase 1 en 4. Wat opvalt in het figuur hieronder is dat pp001 vrijwel even hoog scoort op roze (negatieve) en blauwe(positieve) items ongeacht of pp001 in fase 2 of 3 is. Het is dus niet zo dat pp001 in fase 3 veel meer lichamelijke klachten of spanning heeft of duidelijk minder activiteiten onderneemt.
-  # 
-  # 
-  # 
-  # ```{r, message=FALSE, fig.width = 8,  fig.height= 7}
-  # data %>% select(c(Datum, positief, negatief, "csp_dna_fase")) %>%
-  #   gather(c(positief, negatief), key = "Var", value = "Score") %>%
-  #   mutate(pos_neg = case_when(Var %in% positief ~ "positief",
-  #                              Var %in% negatief ~ "negatief")) -> long
-  # #
-  # # ggplot(long, aes(x = fct_reorder(Var, -Score, na.rm = T), y = Score)) +
-  # #   geom_boxplot() +
-  # #   coord_flip() +
-  # #   theme_minimal() +
-  # #   labs(x = NULL) +
-  # #   theme(panel.grid.major.y = element_blank())
-  # #
-  # data %>% filter(!is.na(csp_dna_fase)) %>%
-  #   group_by(csp_dna_fase) %>% summarise(n_fase = n()) -> n_fase
-  # 
-  # ggplot(filter(long, !is.na(csp_dna_fase)),
-  #        aes(x = fct_reorder(Var, -Score, na.rm = T), y = Score,
-  #            colour = pos_neg)) +
-  #   geom_point(stat = "summary", fun.y = "mean", size = 4) +
-  #   geom_label(data = n_fase, aes(x = -Inf, y = -Inf, label = paste("n =", n_fase)),
-  #              inherit.aes = FALSE, hjust = -0.1, vjust = -0.1, size = 6) +
-  #   scale_colour_manual(breaks = c("positief", "negatief"), values = c("#CC79A7", "#56B4E9")) +
-  #   guides(colour=FALSE) +
-  #   coord_flip() +
-  #   theme_linedraw() +
-  #   labs(x = NULL, colour = NULL) +
-  #   theme(panel.grid.major.y = element_blank(),
-  #         axis.text = element_text(size = 12)) +
-  #   facet_grid(~ csp_dna_fase)
-  # 
-  # 
-  # #ggplot(long, aes(x = Score)) +
-  # #  geom_histogram(binwidth = 10) +
-  # #  theme_minimal() +
-  # #  facet_wrap(~ fct_reorder(Var, Score, na.rm = T)) +
-  # #  theme(panel.grid.minor = element_blank(),
-  # #        panel.grid.major.x = element_blank())
-  # ```
+  # Vector with variable names of negative emotions
+  negatief <- c("Bedroefd", "Boos", "Bang", "Energie", "Spanning", "Zelfbeeld",
+                "Terugtrekken", "Destructief", "Suicidaliteit", "Onrustig", "Bijzondere_ervaringen",
+                "Negatief_contact", "Lichamelijke_klachten", "Onplezierig")
+  
+  # Vector with variable names of positive emotions
+  positief <- c("Blij", "Ondernemen", "Contact_behoefte", "Ervaren_controle",
+                "Zorg_zelf", "Activiteiten", "Verplichtingen", "Plezierig")
+
+  # Create long dataframe
+  long <- data %>%
+    select(all_of(c("Datum", "csp_dna_55a", "csp_dna_56a",
+                    "csp_dna_57a", "csp_dna_77a", "csp_dna_78a","csp_dna_fase", "dayno", "pertwee",
+                    positief, negatief))) %>%
+    gather(all_of(c(positief, negatief)), key = "Var", value = "Score") %>%
+    mutate(pos_neg = case_when(Var %in% positief ~ "positief",
+                               Var %in% negatief ~ "negatief"),
+           csp_dna_fase = factor(csp_dna_fase, levels = c(1, 2, 3, 4)))
+  
+  # Create dataframe with sample sizes for each fase
+  n_fase_grid <- data %>%
+    mutate(csp_dna_fase = factor(csp_dna_fase, levels = c(1, 2, 3, 4))) %>% 
+    filter(!is.na(csp_dna_fase)) %>%
+    group_by(csp_dna_fase, .drop = FALSE) %>% 
+    summarise(n_fase = n()) %>% 
+    mutate(labels = paste0(csp_dna_fase, " ( ", n_fase, " )"))
+  
+  # Vector with labels for facets
+  to_string <- as_labeller(c(`1` = n_fase_grid$labels[1],
+                             `2` = n_fase_grid$labels[2],
+                             `3` = n_fase_grid$labels[3],
+                             `4` = n_fase_grid$labels[4]))
+  
+  ggplot(filter(long, !is.na(csp_dna_fase)),
+         aes(x = fct_reorder(Var, -Score, na.rm = T), y = Score,
+             colour = pos_neg)) +
+    geom_point(stat = "summary", fun = "mean", size = 4) +
+    scale_colour_manual(breaks = c("positief", "negatief"), values = c("#CC79A7", "#56B4E9")) +
+    guides(colour = "none") +
+    scale_y_continuous(limits = c(0, 100), guide = guide_axis(n.dodge = 3)) +
+    facet_grid(.~csp_dna_fase, labeller = to_string, drop = FALSE) +
+    theme_linedraw() +
+    labs(y = "gemiddelde score", title = "Gemiddelde score per gedraging per 'fase'", x = NULL) +
+    theme(panel.grid.minor = element_blank(),
+          panel.grid.major.y = element_blank(),
+          axis.text.x = element_text(size = 12),
+          axis.text.y = element_text(size = 8)) +
+    coord_flip() -> grid_gg # Store as ggplot object
+  
+  # This "hack" is needed to give facets different colours
+  g <- ggplot_gtable(ggplot_build(grid_gg))
+  strip_both <- which(grepl('strip-', g$layout$name))
+  fills <- c("green3", "yellow2", "darkorange", "firebrick2")
+  k <- 1
+  for (i in strip_both) {
+    j <- which(grepl('rect', g$grobs[[i]]$grobs[[1]]$childrenOrder))
+    g$grobs[[i]]$grobs[[1]]$children[[j]]$gp$fill <- fills[k]
+    k <- k+1
+  }
+  
+  if(output_format == "ggplot") {
+    grid.draw(g)
+  } else {
+    svg(file = "viz_report_behaviour.svg", height = 7.5, width = 5)
+    grid.draw(g)
+    dev.off()
+  }
 
 }
