2a830986afa5da65b20f080880796706c239acd3
‘Gert <g.stulp@rug.nl>
2023-11-30 20:53:25 +0100

    updated viewbox

--- R/error_to_svg.R
+++ R/error_to_svg.R
@@ -6,7 +6,7 @@
 #' @export
 error_to_svg <- function(input) {
   paste0(
-    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 480"><g>',
+    '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 800 90"><g>',
     '<rect fill="#ffffff" stroke="red" stroke-width="2" x="5" y="5" width="780" height="80" />',
     '<text x="10" y="50" font-size="24" fill="red">', 
     'GEEN GRAFIEKEN GEGENEREERD [',
